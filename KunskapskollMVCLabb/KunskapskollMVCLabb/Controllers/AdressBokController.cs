67fd6ec9e51fa1132faebe207f1af383f31c22e5
Therese Sjögren <Therese@DESKTOP-UR5CQ7P>
2016-11-28 15:23:31 +0100

    testing

--- KunskapskollMVCLabb/KunskapskollMVCLabb/Controllers/AdressBokController.cs
+++ KunskapskollMVCLabb/KunskapskollMVCLabb/Controllers/AdressBokController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KunskapskollMVCLabb.Models;
 
 namespace KunskapskollMVCLabb.Controllers
 {
     public class AdressBokController : Controller
     {
-        
+        static List<AdressBok> adressBok = new List<AdressBok>();
         public ActionResult Index()
         {
-            return View();
+            return View(adressBok);
+        }
+        public ActionResult Create(AdressBok adressbok)
+        {
+            adressBok.Add(adressbok);
+            return PartialView(adressbok);
         }
     }
 }
