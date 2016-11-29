04126a8e326e3bce5e126a0ae2e57f631ca9e7b8
Therese Sjögren <Therese@DESKTOP-UR5CQ7P>
2016-11-29 08:51:09 +0100

    Added delete

--- KunskapskollMVCLabb/KunskapskollMVCLabb/Controllers/AdressBokController.cs
+++ KunskapskollMVCLabb/KunskapskollMVCLabb/Controllers/AdressBokController.cs
@@ -12,12 +12,26 @@ namespace KunskapskollMVCLabb.Controllers
         static List<AdressBok> adressBok = new List<AdressBok>();
         public ActionResult Index()
         {
-            return View(adressBok);
+            AdressBok newAdressBok = new AdressBok();
+            return View(newAdressBok);
         }
+        [HttpPost]
         public ActionResult Create(AdressBok adressbok)
         {
+            adressbok.UpdatedDate = DateTime.Now;
             adressBok.Add(adressbok);
-            return View(adressbok);
+            return PartialView("List",adressBok);
+        }
+        public ActionResult ListaAlla()
+        {
+            return PartialView("List", adressBok);
+        }
+        [HttpPost]
+        public ActionResult Delete(Guid id)
+        {
+            var delete = adressBok.First(x => x.AdressBokID == id);
+            adressBok.Remove(delete);
+            return PartialView("List", adressBok);
         }
     }
 }
